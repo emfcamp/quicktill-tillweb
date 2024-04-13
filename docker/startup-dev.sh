346273f2d8e1f3968b0098ab9306ff0219f17f40
Tom Lynch <t.lynch@arts.ac.uk>
2024-04-13 18:24:53 +0100

    Automatically compile SASS once at startup

--- docker/startup-dev.sh
+++ docker/startup-dev.sh
@@ -2,5 +2,6 @@
 
 # Current directory is /app when this script is executed
 
+./manage.py sass emf/static/emf/scss emf/static/emf/css -t compressed
 ./manage.py migrate
 exec ./manage.py runserver 0.0.0.0:8000
