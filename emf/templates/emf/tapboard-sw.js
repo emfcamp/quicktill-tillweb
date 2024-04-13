9789c217399a37fb6ab10abffd71cd1243de272c
Tom Lynch <t.lynch@arts.ac.uk>
2024-04-13 12:45:18 +0100

    Reorganise emf resources

--- emf/templates/emf/tapboard-sw.js
+++ emf/templates/emf/tapboard-sw.js
@@ -5,19 +5,19 @@ const CACHE_NAME = `tillboard-${VERSION}`;
 
 const APP_STATIC_RESOURCES = [
     "{% url "tapboard" %}",
-    "{% static "emf/tapboard.js" %}",
-    "{% static "emf/fonts.css" %}",
-    "{% static "emf/tapboard.css" %}",
-    "{% static "emf/tapboard-bg.png" %}",
-    "{% static "emf/raleway-regular-webfont.woff" %}",
-    "{% static "emf/raleway-regular-webfont.woff2" %}",
-    "{% static "emf/raleway-semibold-webfont.woff" %}",
-    "{% static "emf/raleway-semibold-webfont.woff2" %}",
-    "{% static "emf/tapboard-icon-512x512.png" %}",
+    "{% static "emf/js/tapboard.js" %}",
+    "{% static "emf/css/fonts.css" %}",
+    "{% static "emf/css/tapboard.css" %}",
+    "{% static "emf/img/tapboard-bg.png" %}",
+    "{% static "emf/css/raleway-regular-webfont.woff" %}",
+    "{% static "emf/css/raleway-regular-webfont.woff2" %}",
+    "{% static "emf/css/raleway-semibold-webfont.woff" %}",
+    "{% static "emf/css/raleway-semibold-webfont.woff2" %}",
+    "{% static "emf/img/tapboard-icon-512x512.png" %}",
     "{% static "emf/tapboard-manifest.json" %}",
-    "{% static "emf/tapboard-menu-icon-white.svg" %}",
-    "{% static "emf/tapboard-not-connected.svg" %}",
-    "{% static "emf/caution-icon.svg" %}",
+    "{% static "emf/img/tapboard-menu-icon-white.svg" %}",
+    "{% static "emf/img/tapboard-not-connected.svg" %}",
+    "{% static "emf/img/caution-icon.svg" %}",
 ];
 
 // On install, cache the static resources
